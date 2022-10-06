94b4be64b29a5de650fc53dc928d8e0092015d29
A.Iulian Tanase <alex.t@cobradevs.com>
2022-10-06 16:14:34 +0200

    Dev Version 2

--- src/Clients/Dev/Adapter.php
+++ src/Clients/Dev/Adapter.php
@@ -9,7 +9,7 @@ class Adapter extends AbstractClientAdapter {
     protected $ClientFile = '';
     
     public function getJavascript() {
-        $javascript = '/*Development Widget Redline Tracker*/';
+        $javascript = '/*Development Widget Redline Tracker*/'."\n\n";
         $to_replace = $this->getOptions();
     
         $javascript = str_replace(array_keys($to_replace), array_values($to_replace), $javascript);
